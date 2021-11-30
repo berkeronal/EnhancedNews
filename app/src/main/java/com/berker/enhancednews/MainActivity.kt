28e02431f4ec70b3ce9d476965d7116f7edfc6e2
Berker <berker435@gmail.com>
2021-11-30 16:30:08 +0300

    First commit with <3

---
+++ app/src/main/java/com/berker/enhancednews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.berker.enhancednews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

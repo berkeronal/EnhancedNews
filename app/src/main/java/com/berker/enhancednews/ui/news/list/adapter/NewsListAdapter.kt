34a7ce17246097cf21fd970f5833e045ab0eacaf
Berker <berker435@gmail.com>
2021-12-03 22:16:52 +0300

    Added 2 new viewtypes for RV (Adapter need refactored)

--- app/src/main/java/com/berker/enhancednews/ui/news/list/adapter/NewsListAdapter.kt
+++ app/src/main/java/com/berker/enhancednews/ui/news/list/adapter/NewsListAdapter.kt
@@ -11,8 +11,11 @@ import android.view.animation.AnticipateOvershootInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.constraintlayout.widget.Guideline
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
+import com.berker.enhancednews.databinding.RvNewsListBigImageItemBinding
 import com.berker.enhancednews.databinding.RvNewsListItemBinding
+import com.berker.enhancednews.databinding.RvNewsListNotificationItemBinding
 import com.berker.enhancednews.domain.model.Article
 import com.berker.enhancednews.ui.news.list.extension.setImage
 
@@ -21,24 +24,71 @@ class NewsListAdapter(
 ) : RecyclerView.Adapter<NewsListViewHolder>() {
 
     private var itemList: List<Article> = mutableListOf()
-
+    private val notificationPosition = 6
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListViewHolder {
-        val itemBinding =
-            RvNewsListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return NewsListViewHolder(itemBinding, itemClickListener)
+        when (viewType) {
+            0 -> {
+                val itemBinding =
+                    RvNewsListBigImageItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                return NewsListViewHolder(itemBinding, viewType, itemClickListener)
+            }
+            1 -> {
+                val itemBinding =
+                    RvNewsListItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                return NewsListViewHolder(itemBinding, viewType, itemClickListener)
+            }
+            2 -> {
+                val itemBinding =
+                    RvNewsListNotificationItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                return NewsListViewHolder(itemBinding, viewType, itemClickListener)
+            }
+            else -> {
+                val itemBinding =
+                    RvNewsListItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                return NewsListViewHolder(itemBinding, viewType, itemClickListener)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: NewsListViewHolder, position: Int) {
+        if (itemList.isNullOrEmpty()) {
+            return
+        }
         setFadeAnimation(holder.itemView)
-        holder.bind(itemList[position])
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount(): Int = itemList.size
-
+    override fun getItemCount(): Int = itemList.size + 1
 
     fun updateData(newArticleList: List<Article>) {
         itemList = newArticleList
         notifyDataSetChanged()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0) {
+            return 0
+        }
+        if (position == notificationPosition) {
+            return 2
+        }
+        return 1
     }
 
     private fun setFadeAnimation(view: View) {
@@ -47,32 +97,75 @@ class NewsListAdapter(
         view.startAnimation(anim)
     }
 
+    private fun getItem(position: Int): Article {
+        return when {
+            position == notificationPosition -> {
+                Article("", "", "", "", "", "", "", "")
+            }
+            position > itemList.size - 1 -> {
+                itemList[notificationPosition]
+            }
+            else -> {
+                itemList[position]
+            }
+        }
+    }
 }
 
 
 class NewsListViewHolder(
-    private val itemBinding: RvNewsListItemBinding,
+    private val itemBinding: ViewDataBinding,
+    private val viewTypes: Int,
     private val clickedItem: ((Article) -> Unit)?,
 ) : RecyclerView.ViewHolder(itemBinding.root) {
     fun bind(article: Article) {
-        itemView.setOnClickListener {
-            clickedItem?.invoke(article)
-        }
-        itemBinding.apply {
-            this.article = article
-            if (article.urlToImage.isNullOrBlank()) {
-                changeGlPercentage(
-                    1f,
-                    constraintLayout = clRvItem,
-                    guideLine = glVerticalRight
-                )
-            }else{
-                changeGlPercentage(
-                    0.7f,
-                    constraintLayout = clRvItem,
-                    guideLine = glVerticalRight
-                )
-                imgNews.setImage(article.urlToImage)
+        when (viewTypes) {
+            0 -> {
+                itemView.setOnClickListener {
+                    clickedItem?.invoke(article)
+                }
+                (itemBinding as RvNewsListBigImageItemBinding).apply {
+                    this.article = article
+                    if (article.urlToImage.isNullOrBlank()) {
+                        changeGlPercentage(
+                            1f,
+                            constraintLayout = clRvItem,
+                            guideLine = glVerticalRight
+                        )
+                    } else {
+                        changeGlPercentage(
+                            0.7f,
+                            constraintLayout = clRvItem,
+                            guideLine = glVerticalRight
+                        )
+                        imgNews.setImage(article.urlToImage)
+                    }
+                }
+            }
+            1 -> {
+                itemView.setOnClickListener {
+                    clickedItem?.invoke(article)
+                }
+                (itemBinding as RvNewsListItemBinding).apply {
+                    this.article = article
+                    if (article.urlToImage.isNullOrBlank()) {
+                        changeGlPercentage(
+                            1f,
+                            constraintLayout = clRvItem,
+                            guideLine = glVerticalRight
+                        )
+                    } else {
+                        changeGlPercentage(
+                            0.7f,
+                            constraintLayout = clRvItem,
+                            guideLine = glVerticalRight
+                        )
+                        imgNews.setImage(article.urlToImage)
+                    }
+                }
+            }
+            2 -> {
+
             }
         }
     }

cf37bc78c3a0dff46e09e9cae339782691b4a380
Berker <berker435@gmail.com>
2021-12-04 20:30:33 +0300

    Menus are added (bottom nav not active)

--- app/src/main/java/com/berker/enhancednews/ui/MainActivity.kt
+++ app/src/main/java/com/berker/enhancednews/ui/MainActivity.kt
@@ -2,16 +2,22 @@ package com.berker.enhancednews.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.berker.enhancednews.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
         initActionBarBackButton()
     }
 
@@ -19,12 +25,15 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.nav_host_fragment_container)
         NavigationUI.setupActionBarWithNavController(
             this,
-            navController
+            navController,
+            drawerLayout
         )
+        val navView = findViewById<NavigationView>(R.id.navView)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment_container)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
